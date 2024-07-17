564f5cf37c979ec28891a9db307a028747ed4f7f
Muhammed Tareq Aziz <muhammedtareqaziz@Muhammeds-MacBook-Pro.local>
2024-07-17 11:14:05 +0600

    refactored

--- backend/db.js
+++ backend/db.js
@@ -1,27 +1,25 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://nishan988281675:qwerty1223@cluster0.ddhqnuf.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0';
+const mongoose = require("mongoose");
 
 const mongoDB = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connected successfully");
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
 
-        const foodData = await mongoose.connection.db.collection("food_items").find({}).toArray();
-        const categoryData = await mongoose.connection.db.collection("foodcatagory").find({}).toArray();
+    const foodData = await mongoose.connection.db.collection("food_items").find({}).toArray();
+    const categoryData = await mongoose.connection.db.collection("foodcatagory").find({}).toArray();
 
-        global.food_items = foodData;
-        global.food_categories = categoryData;
+    global.food_items = foodData;
+    global.food_categories = categoryData;
 
-        console.log(global.food_items);
-        console.log(global.food_categories);
-
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-        process.exit(1);
-    }
+    console.log(global.food_items);
+    console.log(global.food_categories);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
 };
 
 module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-const mongoDB = require('./db');
+const mongoDB = require("./db");
+require("dotenv").config();
 
 mongoDB();
-
 // Configure CORS
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-}));
+app.use(cors());
 
 app.use(express.json());
 
-app.use('/api', require("./Routes/CreateUser"));
-app.use('/api', require("./Routes/LoginUser"));
-app.use('/api', require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/LoginUser"));
+app.use("/api", require("./Routes/DisplayData"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!---');
+app.get("/", (req, res) => {
+  res.send("Hello World!---");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
